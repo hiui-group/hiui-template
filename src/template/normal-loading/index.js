bd050c6b15c770350b3e863542181391f3a23ccd
solarjoker <chenissolar@gmail.com>
2019-05-28 19:36:49 +0800

    finish loading page

---
+++ src/template/normal-loading/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Loading from '@hi-ui/hiui/es/loading'
+import './index.scss'
+
+const NormalLoading = () => {
+  return (
+    <div className='page page--loading'>
+      <Loading />
+      <div className='loading-text'>正在努力加载中...</div>
+    </div>
+  )
+}
+
+export default NormalLoading

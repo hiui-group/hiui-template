2929b5d3d0a15b8818a0fdbe88a5f4be80e4dbd4
lenorewei <weirong9412@gmail.com>
2019-07-16 17:56:43 +0800

    feat: tile singe and muti update

--- src/template/tile-multiple/index.js
+++ src/template/tile-multiple/index.js
@@ -5,8 +5,14 @@ import Checkbox from '@hi-ui/hiui/es/checkbox'
 import Button from '@hi-ui/hiui/es/button'
 import { DataFilter } from '@hi-ui/component-kit/es/data-filter'
 import Icon from '@hi-ui/hiui/es/icon'
+import Modal from '@hi-ui/hiui/es/modal'
+import Dropdown from '@hi-ui/hiui/es/dropdown'
+import { handleNotificate } from '@hi-ui/hiui/es/notification'
 import './index.scss'
-
+const orderPlatformList = ['小米商城', '小米之家', '天猫旗舰店', '京东旗舰店']
+const orderDeliveryList = ['顺丰', 'EMS', '如风达', '百世汇通', '自取']
+const orderPaymentList = ['微信支付', '支付宝', '银联', '信用卡', '现金']
+const orderStatusList = ['待支付', '已支付', '配货中', '配送中', '已收货', '已取消', '已关闭']
 export default class Template extends Component {
   constructor (props) {
     super(props)
@@ -18,131 +24,106 @@ export default class Template extends Component {
         }
       },
       action: {
-        render: () => (
+        render: (key, row) => (
           <React.Fragment>
-            <Icon name='edit' />
-            <Icon name='close' />
-            <Icon name='more' />
+            <Link to='/form/form-basic' className='hi-tpl__add' ><Icon name='edit' /></Link>
+            <span onClick={this.showDelModal.bind(this, row)} className='action-del'><Icon name='close' /></span>
+            <span className='action-more'><Dropdown list={[{ title: '打印小票' }]} title='更多' onClick={(val) => console.log(val)} /></span>
           </React.Fragment>
         )
       }
     }
 
     this.state = {
-      field1: {
-        list: [
-          {
-            text: '订单A',
-            value: '订单A'
-          },
-          {
-            text: '订单B',
-            value: '订单B'
-          },
-          {
-            text: '订单C',
-            value: '订单C'
-          },
-          {
-            text: '订单D',
-            value: '订单D'
-          },
-          {
-            text: '订单E',
-            value: '订单E'
-          }
-        ]
-      },
-      field2: {
-        list: [
-          {
-            text: '小米商城',
-            value: '小米商城'
-          },
-          {
-            text: '小米之家',
-            value: '小米之家'
-          },
-          {
-            text: '天猫旗舰店',
-            value: '天猫旗舰店'
-          },
-          {
-            text: '京东旗舰店',
-            value: '京东旗舰店'
-          }
-        ]
-      },
-      field3: {
-        list: [
-          {
-            text: '顺丰',
-            value: '顺丰'
-          },
-          {
-            text: 'EMS',
-            value: 'EMS'
-          },
-          {
-            text: '自取',
-            value: '自取'
-          },
-          {
-            text: '圆通',
-            value: '圆通'
-          },
-          {
-            text: '申通',
-            value: '申通'
-          },
-          {
-            text: '中通',
-            value: '中通'
-          }
-        ]
-      },
+      field1: [],
+      field2: [],
+      field3: [],
+      field4: [],
       pageSize: 10,
       forms: this.initForms()
     }
+  }
+
+  componentDidMount () {
+    let orderPlatformState = orderPlatformList.map(item => ({
+      text: item,
+      value: item
+    }))
+    let orderDeliveryState = orderDeliveryList.map(item => ({
+      text: item,
+      value: item
+    }))
+    let orderPaymentState = orderPaymentList.map(item => ({
+      text: item,
+      value: item
+    }))
+    let orderStatusState = orderStatusList.map(item => ({
+      text: item,
+      value: item
+    }))
+    this.setState({
+      field1: orderPlatformState,
+      field2: orderDeliveryState,
+      field3: orderPaymentState,
+      field4: orderStatusState
+    })
   }
 
   initForms () {
     return Object.assign(
       {},
       {
-        column1: '全部',
-        column2: '全部',
-        column3: '全部'
+        order_platform: '全部',
+        order_delivery: '全部',
+        order_payment: '全部',
+        order_status: '全部'
       }
     )
   }
 
-  updateForm (data) {
+  showDelModal (row) {
+    this.setState({
+      delModal: row
+    })
+  }
+
+  cancelEvent () {
+    this.setState({
+      delModal: false
+    })
+  }
+
+  delEvent () {
+    handleNotificate({ type: 'success', duration: 2500, showClose: false, autoClose: true, title: '标题', message: '订单号为' + this.state.delModal.order_id + '已删除' })
+    this.setState({
+      delModal: false
+    })
+  }
+
+  updateForm (data, callback) {
     const forms = Object.assign({}, this.state.forms, data)
 
     this.setState({
       forms
+    }, () => {
+      callback && callback()
     })
   }
 
-  reset () {
-    this.updateForm(this.initForms())
+  reset (callback) {
+    this.updateForm(this.initForms(), callback)
+  }
+
+  getParam (list) {
+    return list.filter(item => item.checked).map(item => item.value).join(',')
   }
 
   setForm () {
     const forms = {
-      column1: this.state.field1.list
-        .filter(item => item.checked)
-        .map(item => item.value)
-        .join(' '),
-      column2: this.state.field2.list
-        .filter(item => item.checked)
-        .map(item => item.value)
-        .join(' '),
-      column3: this.state.field3.list
-        .filter(item => item.checked)
-        .map(item => item.value)
-        .join(' ')
+      order_platform: this.getParam(this.state.field1),
+      order_delivery: this.getParam(this.state.field2),
+      order_payment: this.getParam(this.state.field3),
+      order_status: this.getParam(this.state.field4)
     }
 
     this.updateForm(forms)
@@ -150,20 +131,22 @@ export default class Template extends Component {
 
   handleResetClick () {
     console.log('-------handleResetClick')
-    const { field1, field2, field3 } = this.state
+    const { field1, field2, field3, field4 } = this.state
 
-    field1.list.forEach(item => (item.checked = false))
-    field2.list.forEach(item => (item.checked = false))
-    field3.list.forEach(item => (item.checked = false))
+    field1.forEach(item => (item.checked = false))
+    field2.forEach(item => (item.checked = false))
+    field3.forEach(item => (item.checked = false))
+    field4.forEach(item => (item.checked = false))
 
     this.setState(
       {
         field1,
         field2,
-        field3
+        field3,
+        field4
       },
       () => {
-        this.reset()
+        this.reset(this.dataFilter.submit(this.initForms()))
       }
     )
   }
@@ -183,7 +166,7 @@ export default class Template extends Component {
           <Col span={24}>
             <DataFilter
               ref={node => (this.dataFilter = node)}
-              url={`https://easy-mock.com/mock/5c1b42e3fe5907404e6540e9/hiui/table/get-datas`}
+              url={`https://easy-mock.com/mock/5c1b42e3fe5907404e6540e9/hiui/table/tile`}
               params={params}
               columnMixins={this.columnMixins}
               actions={[
@@ -226,12 +209,12 @@ export default class Template extends Component {
               ]}
             >
               <Row gutter>
-                <div className='block-filter__label block-filter__label--checkbox'>订单类型</div>
+                <div className='block-filter__label block-filter__label--checkbox'>订单状态</div>
                 <Col className='checkboxs-group'>
                   <Checkbox
                     all='one'
                     onChange={list => {
-                      const fieldList = this.state.field1.list
+                      const fieldList = this.state.field1
 
                       fieldList.forEach(item => {
                         if (list.indexOf(item.value) > -1) {
@@ -245,7 +228,7 @@ export default class Template extends Component {
                   >
                     全选
                   </Checkbox>
-                  <Checkbox list={this.state.field1.list} name='one' />
+                  <Checkbox list={this.state.field1} name='one' />
                 </Col>
               </Row>
               <Row gutter>
@@ -254,7 +237,7 @@ export default class Template extends Component {
                   <Checkbox
                     all='two'
                     onChange={list => {
-                      const fieldList = this.state.field2.list
+                      const fieldList = this.state.field2
 
                       fieldList.forEach(item => {
                         if (list.indexOf(item.value) > -1) {
@@ -268,7 +251,7 @@ export default class Template extends Component {
                   >
                     全选
                   </Checkbox>
-                  <Checkbox list={this.state.field2.list} name='two' />
+                  <Checkbox list={this.state.field2} name='two' />
                 </Col>
               </Row>
               <Row gutter>
@@ -277,7 +260,7 @@ export default class Template extends Component {
                   <Checkbox
                     all='three'
                     onChange={list => {
-                      const fieldList = this.state.field3.list
+                      const fieldList = this.state.field3
 
                       fieldList.forEach(item => {
                         if (list.indexOf(item.value) > -1) {
@@ -291,12 +274,49 @@ export default class Template extends Component {
                   >
                     全选
                   </Checkbox>
-                  <Checkbox list={this.state.field3.list} name='three' />
+                  <Checkbox list={this.state.field3} name='three' />
+                </Col>
+              </Row>
+
+              <Row gutter>
+                <div className='block-filter__label block-filter__label--checkbox'>支付方式</div>
+                <Col className='checkboxs-group'>
+                  <Checkbox
+                    all='four'
+                    onChange={list => {
+                      const fieldList = this.state.field4
+
+                      fieldList.forEach(item => {
+                        if (list.indexOf(item.value) > -1) {
+                          item.checked = true
+                        } else {
+                          item.checked = false
+                        }
+                      })
+                      this.setForm()
+                    }}
+                  >
+                    全选
+                  </Checkbox>
+                  <Checkbox list={this.state.field4} name='four' />
                 </Col>
               </Row>
             </DataFilter>
           </Col>
         </Row>
+
+        <Modal
+          title='确认'
+          size='small'
+          show={!!this.state.delModal}
+          onCancel={this.cancelEvent.bind(this)}
+          footers={[
+            <Button type='default' key={'cancel'} onClick={this.cancelEvent.bind(this)}>取消</Button>,
+            <Button type='danger' key={'sure'} onClick={this.delEvent.bind(this)}>确认</Button>
+          ]}
+        >
+          <span>确认要删除订单号为{this.state.delModal && this.state.delModal.order_id}的订单么？</span>
+        </Modal>
       </div>
     )
   }
--- src/template/tile-single/index.js
+++ src/template/tile-single/index.js
@@ -3,8 +3,11 @@ import Grid from '@hi-ui/hiui/es/grid'
 import { Link } from 'react-router-dom'
 import { DataFilter } from '@hi-ui/component-kit/es/data-filter'
 import Radio from '@hi-ui/hiui/es/radio'
-import Button from '@hi-ui/hiui/es/button'
 import Icon from '@hi-ui/hiui/es/icon'
+import Modal from '@hi-ui/hiui/es/modal'
+import Button from '@hi-ui/hiui/es/button'
+import Dropdown from '@hi-ui/hiui/es/dropdown'
+import { handleNotificate } from '@hi-ui/hiui/es/notification'
 import './index.scss'
 
 export default class Template extends Component {
@@ -18,11 +21,11 @@ export default class Template extends Component {
         }
       },
       action: {
-        render: () => (
+        render: (key, row) => (
           <React.Fragment>
-            <Icon name='edit' />
-            <Icon name='close' />
-            <Icon name='more' />
+            <Link to='/form/form-basic' className='hi-tpl__add' ><Icon name='edit' /></Link>
+            <span onClick={this.showDelModal.bind(this, row)} className='action-del'><Icon name='close' /></span>
+            <span className='action-more'><Dropdown list={[{ title: '打印小票' }]} title='更多' onClick={(val) => console.log(val)} /></span>
           </React.Fragment>
         )
       }
@@ -30,7 +33,7 @@ export default class Template extends Component {
 
     this.state = {
       field1: {
-        list: ['全部', '订单A', '订单B', '订单C', '订单D', '订单E', '订单F'],
+        list: ['全部', '待支付', '已支付', '配货中', '配送中', '已收货', '已取消', '已关闭'],
         checkIndex: 0
       },
       field2: {
@@ -38,12 +41,36 @@ export default class Template extends Component {
         checkIndex: 0
       },
       field3: {
-        list: ['全部', '顺丰', 'EMS', '中通', '申通', '圆通', '自取'],
+        list: ['全部', '顺丰', 'EMS', '如风达', '百世汇通', '自取'],
+        checkIndex: 0
+      },
+      field4: {
+        list: ['全部', '微信支付', '支付宝', '银联', '信用卡', '现金'],
         checkIndex: 0
       },
       pageSize: 10,
-      forms: this.initForms()
+      forms: this.initForms(),
+      delModal: false
     }
+  }
+
+  showDelModal (row) {
+    this.setState({
+      delModal: row
+    })
+  }
+
+  cancelEvent () {
+    this.setState({
+      delModal: false
+    })
+  }
+
+  delEvent () {
+    handleNotificate({ type: 'success', duration: 2500, showClose: false, autoClose: true, title: '标题', message: '订单号为' + this.state.delModal.order_id + '已删除' })
+    this.setState({
+      delModal: false
+    })
   }
 
   updateForm (data) {
@@ -63,9 +90,10 @@ export default class Template extends Component {
     return Object.assign(
       {},
       {
-        column1: '全部',
-        column2: '全部',
-        column3: '全部'
+        order_status: '全部',
+        order_delivery: '全部',
+        order_platform: '全部',
+        order_payment: '全部'
       }
     )
   }
@@ -74,7 +102,7 @@ export default class Template extends Component {
     const Row = Grid.Row
     const Col = Grid.Col
 
-    const { field1, field2, field3, pageSize, forms } = this.state
+    const { field1, field2, field3, field4, pageSize, forms } = this.state
     const params = {
       pageSize
     }
@@ -85,7 +113,7 @@ export default class Template extends Component {
           <Col span={24}>
             <DataFilter
               ref={node => (this.dataFilter = node)}
-              url={`https://easy-mock.com/mock/5c1b42e3fe5907404e6540e9/hiui/table/get-datas`}
+              url={`https://easy-mock.com/mock/5c1b42e3fe5907404e6540e9/hiui/table/tile`}
               params={params}
               columnMixins={this.columnMixins}
               actions={[
@@ -126,7 +154,7 @@ export default class Template extends Component {
               ]}
             >
               <Row gutter>
-                <div className='block-filter__label block-filter__label--radio'>订单类型</div>
+                <div className='block-filter__label block-filter__label--radio'>订单状态</div>
                 <Radio
                   list={field1.list}
                   checked={field1.checkIndex}
@@ -137,14 +165,14 @@ export default class Template extends Component {
                         field1
                       },
                       () => {
-                        this.updateForm({ column1: data })
+                        this.updateForm({ order_status: data })
                       }
                     )
                   }}
                 />
               </Row>
               <Row gutter>
-                <div className='block-filter__label block-filter__label--radio'>业务来源...</div>
+                <div className='block-filter__label block-filter__label--radio'>业务来源</div>
                 <Radio
                   list={field2.list}
                   checked={field2.checkIndex}
@@ -155,14 +183,14 @@ export default class Template extends Component {
                         field2
                       },
                       () => {
-                        this.updateForm({ column2: data })
+                        this.updateForm({ order_platform: data })
                       }
                     )
                   }}
                 />
               </Row>
               <Row gutter>
-                <div className='block-filter__label block-filter__label--radio'>运输</div>
+                <div className='block-filter__label block-filter__label--radio'>运输方式</div>
                 <Radio
                   list={field3.list}
                   checked={field3.checkIndex}
@@ -173,7 +201,25 @@ export default class Template extends Component {
                         field3
                       },
                       () => {
-                        this.updateForm({ column3: data })
+                        this.updateForm({ order_delivery: data })
+                      }
+                    )
+                  }}
+                />
+              </Row>
+              <Row gutter>
+                <div className='block-filter__label block-filter__label--radio'>支付方式</div>
+                <Radio
+                  list={field4.list}
+                  checked={field4.checkIndex}
+                  onChange={data => {
+                    field4.checkIndex = field4.list.indexOf(data)
+                    this.setState(
+                      {
+                        field3
+                      },
+                      () => {
+                        this.updateForm({ order_payment: data })
                       }
                     )
                   }}
@@ -182,6 +228,18 @@ export default class Template extends Component {
             </DataFilter>
           </Col>
         </Row>
+        <Modal
+          title='确认'
+          size='small'
+          show={!!this.state.delModal}
+          onCancel={this.cancelEvent.bind(this)}
+          footers={[
+            <Button type='default' key={'cancel'} onClick={this.cancelEvent.bind(this)}>取消</Button>,
+            <Button type='danger' key={'sure'} onClick={this.delEvent.bind(this)}>确认</Button>
+          ]}
+        >
+          <span>确认要删除订单号为{this.state.delModal && this.state.delModal.order_id}的订单么？</span>
+        </Modal>
       </div>
     )
   }

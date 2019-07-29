75f3b091fadf3cd152cf353d3be2be814b6d873e
solarjoker <chenissolar@gmail.com>
2019-07-29 18:59:33 +0800

    升级主题

--- src/route/index.js
+++ src/route/index.js
@@ -1,303 +1,211 @@
-import React from 'react'
-import { SiderLayout } from '@hi-ui/classic-theme'
-import { siders } from './siders'
-
-export default [
+const components = {}
+const files = require.context('../template', true, /.js$/)
+files.keys().forEach(key => {
+  let _key = key.split('/')[1]
+  components[_key] = files(key).default
+})
+const config = [
   {
-    path: '/',
-    component: require('../template/home-dashboard').default,
-    exact: true
+    name: '首页',
+    children: [
+      { name: '仪表盘', path: '/home-dashboard' },
+      { name: '门户', path: '/home-portal' },
+      { name: '搜索', path: '/home-search' }
+    ]
   },
   {
-    path: '/home',
-    render: props => {
-      return (
-        <SiderLayout
-          sider={siders.home}
-          accordion={false}
-          routes={[
-            {
-              path: '/home/home-dashboard',
-              component: require('../template/home-dashboard').default
-            },
-            {
-              path: '/home/home-workbench',
-              component: require('../template/home-workbench').default
-            },
-            {
-              path: '/home/home-portal',
-              component: require('../template/home-portal').default
-            },
-            {
-              path: '/home/home-search',
-              component: require('../template/home-search').default
-            }
-          ]}
-          {...props}
-        />
-      )
-    }
+    name: '表单',
+    children: [
+      {
+        name: '单列',
+        path: '/form-basic'
+      },
+      {
+        name: '双列',
+        path: '/form-double-column'
+      },
+      {
+        name: '分组-标签',
+        path: '/form-vertical-group'
+      },
+      {
+        name: '分组-区块',
+        path: '/form-group'
+      },
+      {
+        name: '分组-组合',
+        path: '/form-unfold-group'
+      },
+      {
+        name: '分步骤',
+        path: '/form-with-stepper'
+      },
+      {
+        name: '弹窗表格',
+        path: '/form-modal'
+      }
+    ]
   },
   {
-    path: '/table',
-    render: props => {
-      return (
-        <SiderLayout
-          sider={siders.table}
-          accordion={false}
-          routes={[
-            {
-              path: '/table/common-search',
-              component: require('../template/common-search').default
-            },
-            {
-              path: '/table/common-basic',
-              component: require('../template/common-basic').default
-            },
-            {
-              path: '/table/query',
-              component: require('../template/query').default
-            },
-            {
-              path: '/table/query-basic',
-              component: require('../template/query-basic').default
-            },
-            {
-              path: '/table/toolbar',
-              component: require('../template/toolbar').default
-            },
-            {
-              path: '/table/toolbar-alter',
-              component: require('../template/toolbar-alter').default
-            },
-            {
-              path: '/table/basic',
-              component: require('../template/basic').default
-            },
-            {
-              path: '/table/group-horizontal',
-              component: require('../template/group-horizontal').default
-            },
-            {
-              path: '/table/group-vertical',
-              component: require('../template/group-vertical').default
-            },
-            {
-              path: '/table/tree-single-query',
-              component: require('../template/tree-single-query').default
-            },
-            {
-              path: '/table/tree-single',
-              component: require('../template/tree-single').default
-            },
-            {
-              path: '/table/tree-multiple-query',
-              component: require('../template/tree-multiple-query').default
-            },
-            {
-              path: '/table/tree-multiple',
-              component: require('../template/tree-multiple').default
-            },
-            {
-              path: '/table/tile-single',
-              component: require('../template/tile-single').default
-            },
-            {
-              path: '/table/tile-multiple',
-              component: require('../template/tile-multiple').default
-            }
-          ]}
-          {...props}
-        />
-      )
-    }
+    name: '表格',
+    children: [
+      {
+        name: '通用-搜索',
+        path: '/common-search'
+      },
+      {
+        name: '通用-基础',
+        path: '/common-basic'
+      },
+      { name: '查询-标准', path: '/query' },
+      {
+        name: '查询-基础',
+        path: '/query-basic'
+      },
+      { name: '工具条-标准', path: '/toolbar' },
+      {
+        name: '工具条-固定',
+        path: '/toolbar-alter'
+      },
+      {
+        name: '分组-横向',
+        path: '/group-horizontal'
+      },
+      {
+        name: '分组-纵向',
+        path: '/group-vertical'
+      },
+      {
+        name: '分组-横向',
+        path: '/tree-single-query'
+      },
+      {
+        name: '分组-纵向',
+        path: '/tree-single'
+      },
+      {
+        name: '树形-多选-查询',
+        path: '/tree-multiple-query'
+      },
+      {
+        name: '树形-多选',
+        path: '/tree-multiple'
+      },
+      {
+        name: '平铺-单选',
+        path: '/tile-single'
+      },
+      {
+        name: '平铺-多选',
+        path: '/table/tile-multiple'
+      }
+    ]
   },
   {
-    path: '/form',
-    render: props => {
-      return (
-        <SiderLayout
-          sider={siders.form}
-          accordion={false}
-          routes={[
-            {
-              path: '/form/form-group',
-              component: require('../template/form-group').default
-            },
-            {
-              path: '/form/form-with-stepper',
-              component: require('../template/form-with-stepper').default
-            },
-            {
-              path: '/form/form-unfold-group',
-              component: require('../template/form-unfold-group').default
-            },
-            {
-              path: '/form/form-vertical-group',
-              component: require('../template/form-vertical-group').default
-            },
-            {
-              path: '/form/form-double-column',
-              component: require('../template/form-double-column').default
-            },
-            {
-              path: '/form/form-basic',
-              component: require('../template/form-basic').default
-            },
-            {
-              path: '/form/form-modal',
-              component: require('../template/form-modal').default
-            }
-          ]}
-          {...props}
-        />
-      )
-    }
-  },
-
-  {
-    path: '/user',
-    render: props => {
-      return (
-        <SiderLayout
-          sider={siders.user}
-          accordion={false}
-          routes={[
-            {
-              path: '/user/user-center',
-              component: require('../template/user-center').default
-            },
-            {
-              path: '/user/user-dashboard',
-              component: require('../template/user-dashboard').default
-            }
-          ]}
-          {...props}
-        />
-      )
-    }
-  },
-  // {
-  //   path: '/loading',
-  //   render: props => {
-  //     return (
-  //       <SiderLayout
-  //         sider={siders.loading}
-  //         accordion={false}
-  //         routes={[
-  //           {
-  //             path: '/loading/normal-loading',
-  //             component: require('../template/normal-loading').default
-  //           }
-  //         ]}
-  //         {...props}
-  //       />
-  //     )
-  //   }
-  // },
-  {
-    path: '/result',
-    render: props => {
-      return (
-        <SiderLayout
-          sider={siders.result}
-          accordion={false}
-          routes={[
-            {
-              path: '/result/result-server-error',
-              component: require('../template/result-server-error').default
-            },
-            {
-              path: '/result/result-no-data',
-              component: require('../template/result-no-data').default
-            },
-            {
-              path: '/result/result-no-auth',
-              component: require('../template/result-no-auth').default
-            },
-            {
-              path: '/result/result-network-broken',
-              component: require('../template/result-network-broken').default
-            },
-            {
-              path: '/result/result-success',
-              component: require('../template/result-success').default
-            },
-            {
-              path: '/result/result-failed',
-              component: require('../template/result-failed').default
-            }
-          ]}
-          {...props}
-        />
-      )
-    }
+    name: '列表',
+    children: [
+      {
+        name: '嵌入式',
+        path: '/list-embeded'
+      },
+      {
+        name: '信息流',
+        path: '/list-info-flow'
+      },
+      {
+        name: '任务清单',
+        path: '/list-task'
+      },
+      {
+        name: '指标百科',
+        path: '/list-indicator'
+      }
+    ]
   },
   {
-    path: '/list',
-    render: props => {
-      return (
-        <SiderLayout
-          sider={siders.list}
-          accordion={false}
-          routes={[
-            {
-              path: '/list/list-embeded',
-              component: require('../template/list-embeded').default
-            },
-            {
-              path: '/list/list-info-flow',
-              component: require('../template/list-info-flow').default
-            },
-            {
-              path: '/list/list-task',
-              component: require('../template/list-task').default
-            },
-            {
-              path: '/list/list-indicator',
-              component: require('../template/list-indicator').default
-            }
-          ]}
-          {...props}
-        />
-      )
-    }
+    name: '详情页',
+    children: [
+      {
+        name: '单列',
+        path: '/detail-basic'
+      },
+      {
+        name: '双列',
+        path: '/detail-double-column'
+      },
+      {
+        name: '分组',
+        path: '/detail-group'
+      },
+      {
+        name: '步骤',
+        path: '/detail-stepper'
+      },
+      {
+        name: '关联',
+        path: '/detail-relevance'
+      },
+      {
+        name: '卡片',
+        path: '/detail-card'
+      }
+    ]
   },
   {
-    path: '/detail',
-    render: props => {
-      return (
-        <SiderLayout
-          sider={siders.detail}
-          routes={[
-            {
-              path: '/detail/detail-basic',
-              component: require('../template/detail-basic').default
-            },
-            {
-              path: '/detail/detail-double-column',
-              component: require('../template/detail-double-column').default
-            },
-            {
-              path: '/detail/detail-group',
-              component: require('../template/detail-group').default
-            },
-            {
-              path: '/detail/detail-stepper',
-              component: require('../template/detail-stepper').default
-            },
-            {
-              path: '/detail/detail-relevance',
-              component: require('../template/detail-relevance').default
-            },
-            {
-              path: '/detail/detail-card',
-              component: require('../template/detail-card').default
-            }
-          ]}
-          {...props}
-        />
-      )
-    }
+    name: '个人页',
+    children: [
+      {
+        name: '账号中心',
+        path: '/user-center'
+      },
+      {
+        name: '活动信息-混合',
+        path: '/user-dashboard'
+      }
+    ]
+  },
+  {
+    name: '结果页',
+    children: [
+      {
+        name: '服务器出错',
+        path: '/result-server-error'
+      },
+      {
+        name: '暂无数据',
+        path: '/result-no-data'
+      },
+      {
+        name: '暂无权限',
+        path: '/result-no-auth'
+      },
+      {
+        name: '网络中断',
+        path: '/result-network-broken'
+      },
+      {
+        name: '成功',
+        path: '/result-success'
+      },
+      {
+        name: '失败',
+        path: '/result-failed'
+      }
+    ]
   }
 ]
+
+const transformConfig = (config, parentId) => {
+  config.forEach((c, index) => {
+    if (c.path) {
+      c.component = components[c.path.split('/')[1]]
+    }
+    c.id = parentId || parentId === 0 ? `${parentId}-${index}` : index
+    if (c.children) {
+      transformConfig(c.children, c.id)
+    }
+  })
+  return config
+}
+
+export default transformConfig(config)

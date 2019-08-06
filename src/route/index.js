32494e10ca5b75d824a7b63fc5eef853e93fe6ac
chenxian <chenxian@xiaomi.com>
2019-08-06 15:26:37 +0800

    update template

--- src/route/index.js
+++ src/route/index.js
@@ -195,17 +195,4 @@ const config = [
   }
 ]
 
-const transformConfig = (config, parentId) => {
-  config.forEach((c, index) => {
-    if (c.path) {
-      c.component = components[c.path.split('/')[1]]
-    }
-    c.id = parentId || parentId === 0 ? `${parentId}-${index}` : index
-    if (c.children) {
-      transformConfig(c.children, c.id)
-    }
-  })
-  return config
-}
-
-export default transformConfig(config)
+export default config

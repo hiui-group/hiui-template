51cbfb302195794e13568dba50bb41d33e3076d1
zhangjunjie <zhangjunjie1@xiaomi.com>
2019-11-27 16:21:54 +0800

    Updates

---
+++ precache-manifest.825237407d182a7cb206cab9154fece1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea7497a04d4db01fee71",
+    "url": "/static/js/main.ea7497a0.chunk.js"
+  },
+  {
+    "revision": "7c58b000a051de1bfa4b",
+    "url": "/static/js/1.7c58b000.chunk.js"
+  },
+  {
+    "revision": "ea7497a04d4db01fee71",
+    "url": "/static/css/main.1a6c1daa.chunk.css"
+  },
+  {
+    "revision": "7c58b000a051de1bfa4b",
+    "url": "/static/css/1.d369ccce.chunk.css"
+  },
+  {
+    "revision": "3ac18ea58ecc658781facdf2aa174389",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hiui-template/precache-manifest.dffb294cf109f1b44237b66add2f5e99.js"
+  "/precache-manifest.825237407d182a7cb206cab9154fece1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hiui-template/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

9da2bf51260361fe51b92c22acf5758e5acba959
laoyao <ordinary_person@outlook.com>
2020-11-24 17:45:34 +0800

    fix: 完成流程卡片页面stepper添加

--- src/template/list-flow-card/index.js
+++ src/template/list-flow-card/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Input, Icon, Button } from '@hi-ui/hiui'
+import { Input, Icon, Button, Stepper } from '@hi-ui/hiui'
 import './index.scss'
 
 export default class ListFlowCard extends Component{
@@ -7,6 +7,7 @@ export default class ListFlowCard extends Component{
     state = {
         searchValue: ''
     }
+
     // 渲染顶部搜索栏
     renderTopPart = () => {
         const { searchValue } = this.state
@@ -38,11 +39,38 @@ export default class ListFlowCard extends Component{
         )
     }
 
+    renderStepper = () => {
+        const data = [
+            {
+                title:'启动阶段',
+                content: '明确项目目标和愿景'
+            },
+            {
+                title:'计划阶段',
+                content: '项目进行中遇到的问题'
+            },
+            {
+                title:'执行阶段',
+                content: '项目过程中每个环节记录'
+            },
+            {
+                title:'维护阶段',
+                content: '借助项目记录了解项目'
+            }
+        ]
+        return (
+            <div className="stepper-container">
+                <Stepper data={data} current={0} itemLayout="vertical"/>
+            </div>
+        )
+    }
+
     render(){
 
         return (
             <div className="page page--list-flow-card">
                 {this.renderTopPart()}
+                {this.renderStepper()}
             </div>
         )
     }

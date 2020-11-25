73eda97c207f50c9c6d4c4c44d11eb987ceabc4a
laoyao <ordinary_person@outlook.com>
2020-11-25 11:02:31 +0800

    fix: 流程卡片,大致完成

--- src/template/list-flow-card/index.js
+++ src/template/list-flow-card/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
-import { Input, Icon, Button, Stepper } from '@hi-ui/hiui'
+import Axios from 'axios'
+import { Input, Icon, Button, Stepper, Grid, Card } from '@hi-ui/hiui'
 import './index.scss'
 
+const { Row, Col } = Grid
 export default class ListFlowCard extends Component{
 
     state = {
-        searchValue: ''
+        searchValue: '',
+        flowInfos: []
+    }
+
+    async componentDidMount(){
+        const { data : { data: {flowInfos = []}}} = await Axios.get('http://mock.be.mi.com/mock/2532/list/flowCard/info')
+        this.setState({flowInfos})
     }
 
     // 渲染顶部搜索栏
@@ -19,6 +27,7 @@ export default class ListFlowCard extends Component{
                         style={{ width: '259px' }}
                         value={searchValue}
                         append={
+                            /* 搜索功能可以前端实现也可以后端实现，根据实际场景而定 */
                             <Button>
                                 <Icon name='search' />
                             </Button>
@@ -30,6 +39,7 @@ export default class ListFlowCard extends Component{
                         }}
                         placeholder='请输入搜索关键词'
                     />
+                    {/* 新建功能也需要自行拓展定义 */}
                     <Button type="primary" className="add-new-button">
                         <Icon name='plus' />
                         新建
@@ -60,8 +70,50 @@ export default class ListFlowCard extends Component{
         ]
         return (
             <div className="stepper-container">
+                {/* 此处只为展示用法，故而current固定住了，应用中应该是动态的，其为数组下标 */}
                 <Stepper data={data} current={0} itemLayout="vertical"/>
             </div>
+        )
+    }
+
+    renderFlowCards = () => {
+        const { flowInfos } = this.state
+
+        const renderStepLines = () => {
+            const lines = []
+            for(let index = 0;index < 4;index++){
+                const lineInfo = flowInfos.filter(item => item.stepId === index)
+                const line = (
+                    <Col key={index} span={6} gutter>
+                        {
+                            lineInfo.map(({id,title,detail}) => {
+                                return (
+                                    <div className="card-container">
+                                        <Card title={title} key={id} 
+                                            extra={
+                                                <span>
+                                                    {/* 功能按钮需用户自己去定制，调用后端接口和后续前端操作 */}
+                                                    <Icon className="card-function-button" name="delete"/>
+                                                    <Icon className="card-function-button" name="ellipsis"/>
+                                                </span>
+                                            }>
+                                            <p className="card-detail">{detail}</p>
+                                        </Card>
+                                    </div>
+                                )
+                            })
+                        }
+                    </Col>
+                )
+                lines.push(line)
+            }
+            return lines
+        }
+
+        return (
+            <Row gutter>
+                {renderStepLines()}
+            </Row>
         )
     }
 
@@ -71,6 +123,7 @@ export default class ListFlowCard extends Component{
             <div className="page page--list-flow-card">
                 {this.renderTopPart()}
                 {this.renderStepper()}
+                {this.renderFlowCards()}
             </div>
         )
     }

2c82c6194fd196cadf3b8a91a6beb6347d8d1e5c
pengrui3 <pengrui3@xiaomi.com>
2021-01-20 21:23:20 +0800

    feat: add eslint check

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  parser: 'babel-eslint',
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true, semi: false, printWidth: 120, trailingComma: 'none' }],
+    'react/prop-types': 0,
+    'react/no-children-prop': 0,
+    'react/display-name': 0
+  }
+}

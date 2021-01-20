f8992e6a5a23176bbe1a33e5238191398f9f8f0f
pengrui3 <pengrui3@xiaomi.com>
2021-01-20 22:40:07 +0800

    feat: add user center page

--- src/template/user-center/index.js
+++ src/template/user-center/index.js
@@ -1,117 +1,108 @@
-import React, { Component } from 'react'
-import '@hi-ui/hiui/es/table/style/index.css'
-import { Form, Input, Button, NavMenu, Cascader, Select } from '@hi-ui/hiui'
-import './index.scss'
+import React, { Component } from "react";
+import "@hi-ui/hiui/es/table/style/index.css";
+import { Form, Input, Button, Tabs, Cascader, Select } from "@hi-ui/hiui";
+import "./index.scss";
 
-const FormItem = Form.Item
+const FormItem = Form.Item;
 const menuList = [
   {
-    title: '基本资料'
+    id: 1,
+    title: "基本资料"
   },
   {
-    title: '偏好设置'
+    id: 2,
+    title: "偏好设置"
   },
   {
-    title: '账户安全'
+    id: 3,
+    title: "账户安全"
   },
   {
-    title: '账户验证'
+    id: 4,
+    title: "账户验证"
   }
-]
+];
 const groups = [
   {
-    id: '信息技术部',
-    content: '信息技术部',
+    id: "信息技术部",
+    content: "信息技术部",
     children: [
       {
-        id: '平台组',
-        content: '平台组',
+        id: "平台组",
+        content: "平台组",
         children: [
           {
-            id: '前端组',
-            content: '前端组'
+            id: "前端组",
+            content: "前端组"
           },
           {
-            id: '测试组',
-            content: '测试组'
+            id: "测试组",
+            content: "测试组"
           }
         ]
       }
     ]
   },
   {
-    id: '云平台',
-    content: '云平台',
+    id: "云平台",
+    content: "云平台",
     children: [
       {
-        id: '小爱',
-        content: '小爱'
+        id: "小爱",
+        content: "小爱"
       },
       {
-        id: 'AI',
-        content: 'AI'
+        id: "AI",
+        content: "AI"
       }
     ]
   }
-]
+];
 const provinces = [
   {
-    id: '湖北',
-    content: '湖北',
+    id: "湖北",
+    content: "湖北",
     children: [
       {
-        id: '武汉',
-        content: '武汉'
+        id: "武汉",
+        content: "武汉"
       },
       {
-        id: '宜昌',
-        content: '宜昌'
+        id: "宜昌",
+        content: "宜昌"
       }
     ]
   }
-]
+];
 const countrys = [
-  { title: '中国', id: '3' },
-  { title: '美国', id: '2' },
-  { title: '日本', id: '4' },
-  { title: '韩国', id: '5' },
-  { title: '英国', id: '6' }
-]
+  { title: "中国", id: "3" },
+  { title: "美国", id: "2" },
+  { title: "日本", id: "4" },
+  { title: "韩国", id: "5" },
+  { title: "英国", id: "6" }
+];
 export default class UserCenter extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      photo: 'https://xiaomi.github.io/hiui/static/img/logo.png?241e0618fe55d933c280e38954edea05',
-      username: 'Mark',
-      nickname: '天天',
+      photo:
+        "https://xiaomi.github.io/hiui/static/img/logo.png?241e0618fe55d933c280e38954edea05",
+      username: "Mark",
+      nickname: "天天",
       cardNum: 10098,
       group: [],
       tel: 18321768907,
-      country: '',
+      country: "",
       province: [],
-      address: ''
-    }
-    this.list = [
-      {
-        title: '基本资料'
-      },
-      {
-        title: '偏好设置'
-      },
-      {
-        title: '账户安全'
-      },
-      {
-        title: '账户验证'
-      }
-    ]
+      address: ""
+    };
   }
 
   onValueChange = (field, value) => {
     this.setState({
       [field]: value
-    })
-  }
+    });
+  };
   submitData = () => {
     const {
       photo,
@@ -123,12 +114,8 @@ export default class UserCenter extends Component {
       country,
       province,
       address
-    } = this.state
-    console.log(photo, username, nickname, cardNum, group, tel, country, province, address)
-    // 请求以上数据
-  }
-  render () {
-    const {
+    } = this.state;
+    console.log(
       photo,
       username,
       nickname,
@@ -138,97 +125,134 @@ export default class UserCenter extends Component {
       country,
       province,
       address
-    } = this.state
+    );
+    // 请求以上数据
+  };
+  render() {
     return (
-      <div className='page--user-center'>
-        <Form labelWidth='144'>
-          <NavMenu onClick={this.handleClick} data={menuList}>
-            <div style={{ width: 592, marginTop: 14, marginLeft: 14 }}>
-              <FormItem label='头像'>
-                <img
-                  alt='头像'
-                  src={photo}
-                  style={{
-                    width: 80,
-                    height: 80
-                  }}
-                />
-              </FormItem>
-              <FormItem label='用户名'>
-                <Input
-                  value={username}
-                  placeholder={'请输入'}
-                  onChange={e => this.onValueChange('username', e.target.value)}
-                />
-              </FormItem>
-              <FormItem label='昵称'>
-                <Input
-                  value={nickname}
-                  placeholder={'请输入'}
-                  onChange={e => this.onValueChange('nickname', e.target.value)}
-                />
-              </FormItem>
-              <FormItem label='工卡号'>
-                <Input
-                  value={cardNum}
-                  placeholder={'请输入'}
-                  onChange={e => this.onValueChange('cardNum', e.target.value)}
-                />
-              </FormItem>
-              <FormItem label='所在部门'>
-                <Cascader
-                  value={group}
-                  onChange={value => {
-                    this.onValueChange('group', value)
-                  }}
-                  data={groups}
-                />
-              </FormItem>
-              <FormItem label='联系方式'>
-                <Input
-                  value={tel}
-                  placeholder={'请输入'}
-                  onChange={e => this.onValueChange('tel', e.target.value)}
-                />
-              </FormItem>
-
-              <FormItem label='国家'>
-                <Select
-                  type='single'
-                  data={countrys}
-                  value={country}
-                  onChange={value => {
-                    this.onValueChange('country', value)
-                  }}
-                />
-              </FormItem>
-              <FormItem label='所在省市'>
-                <Cascader
-                  value={province}
-                  onChange={value => {
-                    this.onValueChange('province', value)
-                  }}
-                  data={provinces}
-                />
-              </FormItem>
-              <FormItem label='详细地址'>
-                <Input
-                  value={address}
-                  placeholder={'请输入'}
-                  onChange={e => this.onValueChange('address', e.target.value)}
-                />
-              </FormItem>
-              <Button
-                type='primary'
-                style={{ marginLeft: '142px', marginTop: '8px' }}
-                onClick={this.submitData}
-              >
-                保存
-              </Button>
-            </div>
-          </NavMenu>
+      <div className="page--user-center">
+        <h2 className="page--user-center_title">账号中心</h2>
+        <Form labelWidth="144">
+          <Tabs
+            className="page--user-center_tabs"
+            type="line"
+            onTabClick={(_, idx) => {
+              this.setState({
+                activeNavMenuIndex: idx
+              });
+            }}
+          >
+            {menuList.map((item) => {
+              return (
+                <Tabs.Pane key={item.id} tabTitle={item.title} tabId={item.id}>
+                  <BasicUserSettings {...this.state} submitData={this.submitData} onChange={this.onValueChange} />
+                </Tabs.Pane>
+              );
+            })}
+          </Tabs>
+          <Tabs onClick={this.handleClick} data={menuList}></Tabs>
         </Form>
       </div>
-    )
+    );
   }
+}
+
+function BasicUserSettings({
+  photo,
+  username,
+  nickname,
+  cardNum,
+  group,
+  tel,
+  country,
+  province,
+  address,
+  submitData,
+  onChange
+}) {
+  return (
+    <div style={{ width: 592, marginTop: 24 }}>
+      <FormItem label="头像">
+        <img
+          alt="头像"
+          src={photo}
+          style={{
+            width: 80,
+            height: 80
+          }}
+        />
+      </FormItem>
+      <FormItem label="用户名">
+        <Input
+          value={username}
+          placeholder={"请输入"}
+          onChange={e => onChange("username", e.target.value)}
+        />
+      </FormItem>
+      <FormItem label="昵称">
+        <Input
+          value={nickname}
+          placeholder={"请输入"}
+          onChange={e => onChange("nickname", e.target.value)}
+        />
+      </FormItem>
+      <FormItem label="工卡号">
+        <Input
+          value={cardNum}
+          placeholder={"请输入"}
+          onChange={e => onChange("cardNum", e.target.value)}
+        />
+      </FormItem>
+      <FormItem label="所在部门">
+        <Cascader
+          value={group}
+          onChange={value => {
+            onChange("group", value);
+          }}
+          data={groups}
+        />
+      </FormItem>
+      <FormItem label="联系方式">
+        <Input
+          value={tel}
+          placeholder={"请输入"}
+          onChange={e => onChange("tel", e.target.value)}
+        />
+      </FormItem>
+
+      <FormItem label="国家">
+        <Select
+          type="single"
+          data={countrys}
+          value={country}
+          onChange={value => {
+            onChange("country", value);
+          }}
+        />
+      </FormItem>
+      <FormItem label="所在省市">
+        <Cascader
+          value={province}
+          onChange={value => {
+            onChange("province", value);
+          }}
+          data={provinces}
+        />
+      </FormItem>
+      <FormItem label="详细地址">
+        <Input
+          value={address}
+          placeholder={"请输入"}
+          onChange={e => onChange("address", e.target.value)}
+        />
+      </FormItem>
+      <Button
+        type="primary"
+        style={{ marginLeft: "142px", marginTop: "8px" }}
+        onClick={submitData}
+      >
+        保存
+      </Button>
+    </div>
+  );
 }

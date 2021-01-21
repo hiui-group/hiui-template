d2aac0a00d39636ebb50cbba4d886eeccc8fcb90
chenzhangdong <chenzhangdong@xiaomi.com>
2021-01-21 17:28:50 +0800

    feat: small card list page finished

--- src/commons/colors.js
+++ src/commons/colors.js
@@ -2,6 +2,7 @@ export default {
   primary: '#4284F5',
   orange: '#ff6900',
   border: '#d8d8d8',
+  lightBackground: '#FBFBFB',
   red: '#EB5252',
   title: '#333',
   content: '#666',
---
+++ src/template/list-indicator/components/ListHeader/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Button, Input } from '@hi-ui/hiui'
+
+import colors from '../../../../commons/colors'
+
+import './index.scss'
+
+export default class ListHeader extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: props.value
+    }
+  }
+
+  render () {
+    const { onChange = () => {} } = this.props
+    const { value } = this.state
+    return (
+      <div className='page--list-header'>
+          指标管理
+          <div>
+            <Input
+              style={{ width: '259px' }}
+              value={value}
+              onChange={(e) => this.setState({ value: e.target.value })}
+              append={
+                <Button
+                  className='search-btn'
+                  icon='search'
+                  style={{
+                    color: colors.primary,
+                    borderLeft: '1px solid ' + colors.border
+                  }}
+                  onClick={() => {
+                    onChange(value)
+                  }}
+                />
+              }
+              placeholder='请输入搜索关键词'
+            />
+          </div>
+        </div>
+    )
+  }
+}

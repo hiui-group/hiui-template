e29d2520f11badad2961dd1c2f7583267ddf84dd
pengrui3 <pengrui3@xiaomi.com>
2021-02-26 15:44:53 +0800

    update result img

--- src/template/result-no-data-mirabbit/index.js
+++ src/template/result-no-data-mirabbit/index.js
@@ -6,7 +6,7 @@ const NoData = () => {
   return (
     <div className="page page--result-no-data">
       <div className="result--container">
-        <img src="/hiui-template/static/result/2/暂无数据.png" alt="no-data" />
+        <img width="480" src="/hiui-template/static/result/2/暂无数据.png" alt="no-data" />
         <div style={{ fontSize: 18, marginBottom: 24, marginTop: 10 }}>抱歉，当前暂无数据！</div>
         <Button type="line">返回</Button>
       </div>

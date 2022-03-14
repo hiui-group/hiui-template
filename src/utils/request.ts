88d935a4507c97804ef0bc8d48a86a8ca38843a5
xuhuihuang <xuhuihuang@xiaomi.com>
2022-03-14 11:58:18 +0800

    fix: 修正 request 传参

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -5,14 +5,25 @@ const delay = (timeout: number) => new Promise((resolve) => window.setTimeout(re
 /**
  * mock 请求
  * @param url mock 接口地址（暂时从 request-data-cache.ts 中获取）
- * @param timeout mock 网络延迟
+ * @param config mock 配置项
  * @returns {Promise<ResponseBody>}
  */
-export const request = async <T = any>(url: keyof typeof RequestDataCache, timeout = 3000):Promise<ResponseBody<T>> => {
+export const request = async <T = any>(url: keyof typeof RequestDataCache, config?: RequestConfig):Promise<ResponseBody<T>> => {
+  const { timeout = 3000 } = (config || {})
+
   await delay(timeout)
 
   return RequestDataCache[url]
 }
+
+export interface RequestConfig{
+  /**
+   * 在本地模拟时有效，timeout 之后才会返回值
+   * @default 3000
+   */
+  timeout?: number
+}
+
 
 export interface ResponseBody<T = any> {
   code: number

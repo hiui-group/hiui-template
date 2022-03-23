67c4872f0a3d9cb638bfc43450779a46a262d6bd
张健康 <zhangjiankang@xiaomi.com>
2022-03-23 10:58:27 +0800

    add:demo code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,14 @@
 export * from './format'
 export * from './native'
 export * from './request'
+
+export const sleep = (duration: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration)
+  })
+}
+
+let AUTO_INREMENT_COUNT:number = 0;
+export const getAutoInrementId = ():number => {
+  return AUTO_INREMENT_COUNT++;
+}
--- src/views/account-profile/sysset.tsx
+++ src/views/account-profile/sysset.tsx
@@ -1,7 +1,19 @@
-import { Switch } from "@hi-ui/hiui";
+import { Loading, Message, Switch } from "@hi-ui/hiui";
 import { BellFilled, CheckSquareFilled, HeartFilled } from "@hi-ui/icons";
+import { getAutoInrementId, sleep } from "../../utils";
 
 export default () => {
+  const mockSave = async () => {
+    const loadingkey = "loading_" + getAutoInrementId()
+    Loading.open(undefined, { key: loadingkey })
+    await sleep(1000)
+    Loading.close(loadingkey)
+    Message.open({
+      title: '保存成功',
+      type: 'success',
+    })
+  }
+
   return (
     <div className="hi-pro-account-profile-contentbox">
       <div className="hi-pro-account-profile-contentbox-title">系统设置</div>
@@ -9,21 +21,21 @@ export default () => {
         <div className="hi-pro-account-sysset-item">
           <HeartFilled className="hi-pro-account-sysset-item_icon" style={{ backgroundColor: '#FEF0E5', color: "#FE7840" }} />
           <div className="hi-pro-account-sysset-item_cont">
-            <div className="hi-pro-account-sysset-item_title">关注消息<Switch defaultChecked={false} /></div>
+            <div className="hi-pro-account-sysset-item_title">关注消息<Switch defaultChecked={false} onChange={ mockSave } /></div>
             <div className="hi-pro-account-sysset-item_desc">关注的实体动态将以站内信的形式通知</div>
           </div>
         </div>
         <div className="hi-pro-account-sysset-item">
           <BellFilled className="hi-pro-account-sysset-item_icon" style={{ backgroundColor: '#F2F4F7', color: "#C9CED6" }} />
           <div className="hi-pro-account-sysset-item_cont">
-            <div className="hi-pro-account-sysset-item_title">系统消息<Switch defaultChecked={true} /></div>
+            <div className="hi-pro-account-sysset-item_title">系统消息<Switch defaultChecked={true} onChange={ mockSave } /></div>
             <div className="hi-pro-account-sysset-item_desc">系统消息将以站内信的形式通知</div>
           </div>
         </div>
         <div className="hi-pro-account-sysset-item">
           <CheckSquareFilled className="hi-pro-account-sysset-item_icon" style={{ backgroundColor: '#E2F3FE', color: "#237FFA" }} />
           <div className="hi-pro-account-sysset-item_cont">
-            <div className="hi-pro-account-sysset-item_title">待办任务<Switch defaultChecked={false} /></div>
+            <div className="hi-pro-account-sysset-item_title">待办任务<Switch defaultChecked={false} onChange={ mockSave } /></div>
             <div className="hi-pro-account-sysset-item_desc">待办任务将以站内信的形式通知</div>
           </div>
         </div>
